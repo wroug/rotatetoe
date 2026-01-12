f8d55ff245e97aebb8e07eac9ec941f5db8f87eb
wroug <goldcc74@gmail.com>
2026-01-12 22:31:33 +0200

    Deleted the compact version of drawboard

--- modules/drawboard.py
+++ modules/drawboard.py
@@ -1,38 +1,6 @@
 def drawboard(height, width, data, compact=False):
     #nonlocal row, col
-    if compact:
-        board = []
-
-        tmp = "╔═"
-        for i in range(width - 1):  # top border
-            tmp += "╤═"
-        tmp += "╗"
-        board.append(tmp)
-
-        for i in range(height):
-            tmp = f"┊{data[i][0]}"
-            for j in range(width - 1):
-                tmp += f"│{data[i][j+1]}"
-            tmp += "┊"
-            board.append(tmp)
-
-            tmp = "╟─"
-            for j in range(width - 1):  # top border
-                tmp += "┼─"
-            tmp += "╢"
-            board.append(tmp)
-
-        board.pop()
-
-        tmp = "╚═"
-        for i in range(width - 1):  # top border
-            tmp += "╧═"
-        tmp += "╝"
-        board.append(tmp)
-
-        return board
-
-    elif not compact:
+    if not compact:
         board = []
 
         tmp = "╔═══"
