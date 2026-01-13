4545a2053bc9ec4e75b807e5cc3a1017b1eb2409
wroug <goldcc74@gmail.com>
2026-01-13 14:43:20 +0200

    Added base stuff to make commands work

--- modules/commands.py
+++ modules/commands.py
@@ -1,10 +1,16 @@
+import curses
 
-
-def commandinit(display):
-    h = stdscr.getmaxyx()[0]
+def commandinit(disp):
+    disp.attron(curses.A_REVERSE)
+    h, w = disp.getmaxyx()
+    disp.addstr(h-1, 0, " "*(w-1))
+    disp.addch(h-1, 0, ':')
+    disp.move(h-1, 1)
     curses.echo()
-    s = disp.getstr()
+    command = disp.getstr().decode('utf-8').strip()
     curses.noecho()
-    disp.addch(h, 0, ':')
-    disp.move(1,h)
-    command = s.decode("utf-8")
+    disp.attroff(curses.A_REVERSE)
+    disp.addstr(h - 1, 0, " " * (w - 1))
+    # stdscr nor disp work in the return-exec thingy so use xac instead
+    if command == "xray":
+        return "xac.clear(); xray = not xray"
