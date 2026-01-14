fd396e0a59c6140bdbc015b8e1cd7de7820e1ed9
wroug <goldcc74@gmail.com>
2026-01-14 23:00:29 +0200

    Added help command

--- modules/commands.py
+++ modules/commands.py
@@ -16,4 +16,26 @@ def commandinit(disp):
         return "xac.clear(); xray = not xray"
     elif command == "quit":
         return "quit()"
+    elif command == "help":
+        curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_WHITE)
+        curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(7, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        fill(disp, 7, "~")
+        theight, twidth = disp.getmaxyx()
+
+
+        marginy = int(theight * 0.05)
+        marginx = marginy * 2
+
+
+        location = centercoords(0, 0, twidth - 1 - (marginx * 2), twidth)
+        h = drawtextbox(disp, marginy, location, twidth - 1 - (marginx * 2), -1, getmessage("command-help.txt"), 1)
+        choice = loadchoices(disp, "command-help.json", marginy + h - 1, 9, 6)
+
+        fill(disp, 1)
+        return "pass"
+
+
+
+
     else: return "xray = xray"
