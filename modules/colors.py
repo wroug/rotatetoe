b8c4cb9759fe1551fad8327233256a5cc1ad799f
wroug <goldcc74@gmail.com>
2026-01-15 17:31:00 +0200

    Script to define all color pairs in one spot

---
+++ modules/colors.py
@@ -0,0 +1,8 @@
+import curses
+
+def defcolors():
+    curses.init_pair(1, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_WHITE)
+    curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(7, curses.COLOR_BLUE, curses.COLOR_BLACK)
