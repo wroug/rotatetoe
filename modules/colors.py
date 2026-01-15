04de595a25acb68233861690f8bcc02ae202f7f8
wroug <goldcc74@gmail.com>
2026-01-15 21:07:51 +0200

    Created a new color pair

--- modules/colors.py
+++ modules/colors.py
@@ -6,3 +6,4 @@ def defcolors():
     curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_WHITE)
     curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(7, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(8, curses.COLOR_RED, curses.COLOR_WHITE)
