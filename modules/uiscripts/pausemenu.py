adb6bbd972097b295404acf3fc9d6ab25f1195f1
wroug <goldcc74@gmail.com>
2026-01-16 12:54:55 +0200

    Created settings and pause menu

--- modules/uiscripts/pausemenu.py
+++ modules/uiscripts/pausemenu.py
@@ -4,32 +4,36 @@ from modules.getmessage import getmessage
 from modules.cleanup import *
 from modules.uiscripts.settings import *
 
+
 def pausemenu(win):
-    fill(win, 7, "~")
-    theight, twidth = win.getmaxyx()
+    inmenu = True
+    while inmenu:
+        fill(win, 7, "~")
+        theight, twidth = win.getmaxyx()
 
 
-    menuwidth = 20
+        menuwidth = 20
 
-    menuitems = 4
+        menuitems = 4
 
-    marginy = centercoords(0, 0, menuitems+3, theight)
-    marginx = centercoords(0, 0, 20, twidth)
+        marginy = centercoords(0, 0, menuitems+3, theight)
+        marginx = centercoords(0, 0, 20, twidth)
 
-    location = centercoords(0, 0, menuwidth, twidth)
-    h = theight-1-marginy
-    h=drawtextbox(win, marginy, location, menuwidth, -1, "MENU\n "+"\n."*menuitems, 1)
-    win.refresh()
-    choice = loadchoices(win, "pausemenu.json", marginy + 2, location+2, 6)
-    if choice == 0:
-        fill(win)
-        return "pass"
-    elif choice == 1:
-        settings(win)
-    elif choice == 2:
-        fill(win)
-        return 'data = [[" "] * width for _ in range(height)];brow, bcol = 0, 0 #board row & board column'
-    elif choice == 3:
-        cleanup()
-        return "pass"
-    return "pass"
+        location = centercoords(0, 0, menuwidth, twidth)
+        h = theight-1-marginy
+        h=drawtextbox(win, marginy, location, menuwidth, -1, "MENU\n "+"\n."*menuitems, 1)
+        win.refresh()
+        choice = loadchoices(win, "pausemenu.json", marginy + 2, location+2, 6)
+        if choice == 0:
+            fill(win)
+            return "pass"
+        elif choice == 1:
+            settings(win)
+        elif choice == 2:
+            fill(win)
+            return 'restart()'
+        elif choice == 3:
+            cleanup()
+            return "pass"
+
+
--- modules/uiscripts/settings.py
+++ modules/uiscripts/settings.py
@@ -1,5 +1,50 @@
-import curses
+import time
 from modules.uitools import *
+from modules.data import *
+from modules.bigrestart import restart_program
 
 def settings(win):
-    loadmenu(win, "settings.json")
+    tmp = getdata()
+    insettings = True
+    while insettings:
+        choice = loadmenu(win, "settings.json")
+        if choice == 0:
+            tmp["xray"] = not tmp["xray"]
+            setdata(tmp)
+        elif choice == 1:
+            choice = loadmenu(win, "needsrestart.json")
+            if choice == 1:
+                a,b,c=True,True,True
+                while a:
+                    try:
+                        tmp["height"] = int(inputbox(win, "Set conveyor height:",24))
+                        setdata(tmp)
+                        a = False
+                    except ValueError:
+                        displayerror(win, "ENTER A NUMBER")
+                while b:
+                    try:
+                        tmp["width"] = int(inputbox(win, "Set conveyor width:",24))
+                        setdata(tmp)
+                        b = False
+                    except ValueError:
+                        displayerror(win, "ENTER A NUMBER")
+                while c:
+                    try:
+                        tmp["gameareawidth"] = int(inputbox(win, "Set game area width:", 24))
+                        setdata(tmp)
+                        c = False
+                    except ValueError:
+                        displayerror(win, "ENTER A NUMBER")
+                restart_program()
+        elif choice == 2:
+            insettings=False
+        elif choice == 3:
+            choice = loadmenu(win, "needsrestart.json")
+            if choice == 1:
+                resetdata()
+                fill(win, 7, letter="~")
+                drawtextbox(win,text="Done!")
+                win.refresh()
+                time.sleep(0.9)
+            restart_program()
